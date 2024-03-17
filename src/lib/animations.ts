e8e378212768ffb2694154cf50525c37b8207eb4
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-17 17:47:40 -0300

    create animation fadeUp and fadeDown

---
+++ src/lib/animations.ts
@@ -0,0 +1,11 @@
+export const fadeUpAnimation = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 }
+}
+
+export const fadeDownAnimation = {
+  initial: { opacity: 0, y: -20 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -20 }
+}
