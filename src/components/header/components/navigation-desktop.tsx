6fba3ce0fccb7c6dfd1d5bcd058849e3ebf4dfff
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-17 17:47:14 -0300

    update component NavigationDesktop

--- src/components/header/components/navigation-desktop.tsx
+++ src/components/header/components/navigation-desktop.tsx
@@ -6,25 +6,41 @@ import {
   NavigationMenuLink,
   NavigationMenuList
 } from '@/components/ui/navigation-menu'
+import { fadeDownAnimation } from '@/lib/animations'
+import { motion } from 'framer-motion'
 import { NavigationLinkType } from '../types'
 
 type NavigationDesktopProps = {
   links: NavigationLinkType[]
 }
 export function NavigationDesktop({ links }: NavigationDesktopProps) {
+  const AnimationNavigationMenuItem = motion(NavigationMenuItem)
+  const AnimationNavigationMenuList = motion(NavigationMenuList)
+
   return (
     <NavigationMenu className='ml-4 hidden flex-1 lg:block'>
-      <NavigationMenuList className='flex items-center justify-end gap-8'>
-        {links.map(link => (
-          <NavigationMenuItem asChild key={link.id}>
+      <AnimationNavigationMenuList
+        transition={{ duration: 1, delay: 0.5 }}
+        className='flex items-center justify-end gap-8'>
+        {links.map((link, i) => (
+          <AnimationNavigationMenuItem
+            asChild
+            key={link.id}
+            /*
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            */
+            {...fadeDownAnimation}
+            transition={{ duration: 0.5, delay: 0.05 * i }}>
             <NavigationMenuLink
               href={link.url}
               className='text-base text-dark hover:text-primary'>
               {link.name}
             </NavigationMenuLink>
-          </NavigationMenuItem>
+          </AnimationNavigationMenuItem>
         ))}
-      </NavigationMenuList>
+      </AnimationNavigationMenuList>
     </NavigationMenu>
   )
 }
