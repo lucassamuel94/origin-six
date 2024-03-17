bc393f7363945d0060289a6a9164ca0c6c56e6aa
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-17 17:47:56 -0300

    add component Hero and Divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from '@/components/header'
+import { Divider } from '@/components/ui/divider'
+import { Hero } from './components/hero'
 
 export default function HomeApp() {
   return (
     <main>
       <Header />
+      <Hero />
+      <Divider direction='right' />
     </main>
   )
 }
