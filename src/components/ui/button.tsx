4b0a0fced711056bf047859e63646601791f2ad2
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-18 18:45:58 -0300

    animation: add component Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,6 +4,8 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
+import { motion } from 'framer-motion'
+
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm lg:text-base font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary/80 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
@@ -38,11 +40,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
+      <motion.div whileTap={{ scale: 1.05 }}>
+        <Comp
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
     )
   }
 )
