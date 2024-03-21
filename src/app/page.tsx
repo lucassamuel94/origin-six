ee4c0260c872eeaae21d13e24ab1f54e7366e5d5
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-21 14:32:04 -0300

    add component Services and Divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from '@/components/header'
 import { Divider } from '@/components/ui/divider'
 import { About } from './components/about'
 import { Hero } from './components/hero'
+import { Services } from './components/services'
 
 export default function HomeApp() {
   return (
@@ -11,6 +12,8 @@ export default function HomeApp() {
       <Divider direction='right' />
       <About />
       <Divider direction='left' />
+      <Services />
+      <Divider direction='right' />
     </main>
   )
 }
