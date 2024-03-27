df2dbf0c4400f65dd7f9505aa43e32ce712be6a8
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-27 13:38:50 -0300

    config: add boxShadow carousel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,9 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      boxShadow: {
+        carousel: '0px 0px 12px rgba(0,0,0,0.08)'
       }
     }
   },
